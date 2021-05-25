980fb43bf7f5fca3ee1301dd32a28f90cf59aa15
uduse <i@uduse.com>
2021-05-25 05:29:37 +0000

    Update.

--- moozi/utils.py
+++ moozi/utils.py
@@ -2,6 +2,9 @@ import typing
 import random
 
 
+Action = typing.NewType("Action", int)
+
+
 class ReplayBuffer(object):
     def __init__(self, capacity):
         super().__init__()
@@ -23,8 +26,11 @@ class ReplayBuffer(object):
 
     def sample(self, num_samples):
         if len(self._data) < num_samples:
-            raise ValueError("{} elements could not be sampled from size {}".format(
-                num_samples, len(self._data)))
+            raise ValueError(
+                "{} elements could not be sampled from size {}".format(
+                    num_samples, len(self._data)
+                )
+            )
         return random.sample(self._data, num_samples)
 
     def __len__(self):
@@ -32,32 +38,3 @@ class ReplayBuffer(object):
 
     def __iter__(self):
         return iter(self._data)
-
-
-Action = typing.NewType('Action', int)
-
-
-class NetworkOutput(typing.NamedTuple):
-    value: float
-    reward: float
-    policy_logits: typing.Dict[Action, float]
-    hidden_state: typing.List[float]
-
-
-class Network(object):
-
-    def initial_inference(self, image) -> NetworkOutput:
-        # representation + prediction function
-        return NetworkOutput(0, 0, {}, [])
-
-    def recurrent_inference(self, hidden_state, action) -> NetworkOutput:
-        # dynamics + prediction function
-        return NetworkOutput(0, 0, {}, [])
-
-    def get_weights(self):
-        # Returns the weights of this network.
-        return []
-
-    def training_steps(self) -> int:
-        # How many steps / batches the network has been trained for.
-        return 0

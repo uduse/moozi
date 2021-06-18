2d2ac2ef29f60880369bc86e27b2ee38d0c9f1e5
uduse <i@uduse.com>
2021-06-17 23:38:40 -0600

    Update.

--- moozi/replay.py
+++ moozi/replay.py
@@ -3,6 +3,7 @@ from os import stat
 from typing import Iterable, List, NamedTuple
 
 import chex
+from nptyping import NDArray
 import dm_env
 import numpy as np
 import reverb
@@ -12,18 +13,15 @@ from acme import specs
 from acme import types as acme_types
 from acme.adders.reverb import DEFAULT_PRIORITY_TABLE, ReverbAdder, base
 from acme.adders.reverb import utils as acme_reverb_utils
-from acme.utils import tree_utils as acme_tree_utils
+from acme.utils import tree_utils
 from acme.wrappers import open_spiel_wrapper
 from reverb.trajectory_writer import TrajectoryColumn
 
 import moozi as mz
 
-# def spec_like_to_tensor_spec(paths: Iterable[str], spec: specs.Array):
-#     return tf.TensorSpec.from_spec(spec, name="/".join(str(p) for p in paths))
-
 
 class Observation(NamedTuple):
-    frame: chex.Array
+    frame: np.ndarray
     reward: np.float32
     # legal_actions_mask: chex.Array
     is_first: np.bool
@@ -47,51 +45,37 @@ class Observation(NamedTuple):
 
 
 class Reflection(NamedTuple):
-    action: np.int32
-    root_value: np.float32
-    child_visits: List[float]
-
-
-def _to_tensor_spec(path, spec):
-    return tf.TensorSpec.from_spec(spec, path[0])
+    action: np.ndarray
+    root_value: np.ndarray
+    child_visits: np.ndarray
 
 
 def _prefix_dim(spec, size):
     return spec.replace(shape=(size,) + spec.shape)
 
 
-def make_signature(
-    env_spec: specs.EnvironmentSpec,
-    num_unroll_steps,
-    num_stacked_frames,
-    num_td_steps,
-    max_episode_length,
-):
+def make_signature(env_spec: specs.EnvironmentSpec, max_episode_length):
 
-    reward_size = max(num_unroll_steps + num_td_steps + 1, max_episode_length)
     obs_signature = Observation(
-        frame=_prefix_dim(env_spec.observations.observation, num_stacked_frames),
-        reward=_prefix_dim(env_spec.rewards, reward_size),
-        # legal_actions_mask=specs.Array(
-        #     shape=(env_spec.actions.num_values,), dtype=np.bool
-        # ),
-        is_first=specs.Array(shape=(num_unroll_steps + 1,), dtype=np.bool),
-        is_last=specs.Array(shape=(num_unroll_steps + 1,), dtype=np.bool),
+        frame=_prefix_dim(env_spec.observations.observation, max_episode_length),
+        reward=_prefix_dim(env_spec.rewards, max_episode_length),
+        is_first=specs.Array(shape=(max_episode_length,), dtype=np.bool),
+        is_last=specs.Array(shape=(max_episode_length,), dtype=np.bool),
     )
 
-    bootstrap_room_left = max(max_episode_length - num_td_steps - 1, 0)
-    root_value_size = min(bootstrap_room_left, num_td_steps + 1)
-    root_value_shape = (root_value_size,) if root_value_size else (1,)
     ref_signature = Reflection(
-        action=specs.Array(shape=(num_unroll_steps,), dtype=np.int32),
-        root_value=specs.Array(shape=root_value_shape, dtype=np.float32),
+        action=specs.Array(shape=(max_episode_length,), dtype=np.int32),
+        root_value=specs.Array(shape=(max_episode_length,), dtype=np.float32),
         child_visits=specs.Array(
-            shape=(num_unroll_steps + 1, env_spec.actions.num_values), dtype=np.float32
+            shape=(max_episode_length, env_spec.actions.num_values), dtype=np.float32
         ),
     )
 
     merged_signature = {**obs_signature._asdict(), **ref_signature._asdict()}
-    return tree.map_structure(tf.TensorSpec.from_spec, merged_signature)
+    # return tree.map_structure(tf.TensorSpec.from_spec, merged_signature)
+    return tree.map_structure_with_path(
+        lambda p, v: tf.TensorSpec.from_spec(v, p[0]), merged_signature
+    )
 
 
 class MooZiAdder(ReverbAdder):
@@ -112,13 +96,11 @@ class MooZiAdder(ReverbAdder):
         self._num_unroll_steps = num_unroll_steps
         self._num_td_steps = num_td_steps
         self._num_stacked_frames = num_stacked_frames
-        # self._discount = tree.map_structure(np.float32, discount)
-
         self._max_episode_length = max_episode_length
+
         super().__init__(
             client=client,
-            max_sequence_length=self._max_episode_length
-            + 1,  # extra place for dummy padding
+            max_sequence_length=self._max_episode_length,
             max_in_flight_items=max_inflight_items,
             delta_encoded=delta_encoded,
         )
@@ -146,13 +128,6 @@ class MooZiAdder(ReverbAdder):
             )._asdict()
             self._writer.append(partial_padding_step, partial_step=False)
 
-            # create dummy values for referencing
-            dummy_padding_step = {
-                **tree.map_structure(np.zeros_like, last_reflection)._asdict(),
-                **tree.map_structure(np.zeros_like, next_observation)._asdict(),
-            }
-            self._writer.append(dummy_padding_step, partial_step=False)
-
             self._write_last()
             self.reset()
 
@@ -165,47 +140,93 @@ class MooZiAdder(ReverbAdder):
         There are two ways of storing experiences for training.
         One way is to store the entire episode into the replay and create
         """
+        trajectory = tree.map_structure(lambda x: x[:], self._writer.history)
+        self._writer.create_item(DEFAULT_PRIORITY_TABLE, 1.0, trajectory)
 
-        history = self._writer.history
-        last_step_idx = self._writer.history["is_last"][:].numpy().argmax()
-        dummy_step_idx = -1
 
-        assert last_step_idx == len(self._writer.history["is_last"]) - 2
+class ReplaySample(NamedTuple):
+    frame: NDArray[np.float32]
+    reward: NDArray[np.float32]
+    is_first: NDArray[np.bool]
+    is_last: NDArray[np.bool]
+    action: NDArray[np.int32]
+    root_value: NDArray[np.float32]
+    child_visits: NDArray[np.float32]
 
-        for slice_idx in range(last_step_idx):
-            frame_slice = history["frame"][: slice_idx + 1]
-            num_frames_to_pad = self._num_stacked_frames - len(frame_slice)
-            if num_frames_to_pad > 0:
-                dummy_frame = history["frame"][dummy_step_idx]
-                dummy_references = [dummy_frame for _ in range(num_frames_to_pad)]
-                frame_slice = TrajectoryColumn(dummy_references + list(frame_slice))
-            assert len(frame_slice) == self._num_stacked_frames
+    def cast(self) -> "ReplaySample":
+        return ReplaySample(
+            frame=np.asarray(self.frame, dtype=np.float32),
+            reward=np.asarray(self.reward, dtype=np.float32),
+            is_first=np.asarray(self.is_first, dtype=np.bool),
+            is_last=np.asarray(self.is_last, dtype=np.bool),
+            action=np.asarray(self.action, dtype=np.int32),
+            root_value=np.asarray(self.root_value, dtype=np.float32),
+            child_visits=np.asarray(self.child_visits, dtype=np.float32),
+        )
 
-            episode_stop = last_step_idx + 1
-            td_stop = slice_idx + self._num_td_steps + 1
-            reward_slice_stop = max(episode_stop, td_stop)
-            reward_slice = history["reward"][slice_idx:reward_slice_stop]
 
-            episode_stop = last_step_idx + 1
-            unroll_stop = slice_idx + self._num_unroll_steps
-            action_slice_stop = max(episode_stop, unroll_stop)
-            action_slice = history["action"][slice_idx:action_slice_stop]
+class TrainTarget(NamedTuple):
+    frame: NDArray[np.float32]
+    action: NDArray[np.int32]
+    value: NDArray[np.float32]
+    last_reward: NDArray[np.float32]
+    child_visits: NDArray[np.float32]
 
-            full_slice = {
-                "frame": frame_slice,
-                "reward": reward_slice,
-                "action": action_slice,
-            }
+    def cast(self) -> "TrainTarget":
+        return TrainTarget(
+            frame=np.asarray(self.frame, dtype=np.float32),
+            action=np.asarray(self.action, dtype=np.int32),
+            value=np.asarray(self.value, dtype=np.float32),
+            last_reward=np.asarray(self.last_reward, dtype=np.float32),
+            child_visits=np.asarray(self.child_visits, dtype=np.float32),
+        )
 
-            # target_slice = tree.map_structure(lambda x: x[start_idx:end_idx], history)
-            # print(target_slice)
-            self._writer.create_item(DEFAULT_PRIORITY_TABLE, 1, full_slice)
 
-        # method #2
-        # padding_step = tree.map_structure(
-        #     lambda x: np.zeros(x[-1].shape, x[-1].dtype), self._writer.history
-        # )
-        # while self._writer.episode_steps < self._max_sequence_length:
-        #     self._writer.append(padding_step)
-        # trajectory = tree.map_structure(lambda x: x[:], self._writer.history)
-        # self._writer.create_item(DEFAULT_PRIORITY_TABLE, 1, trajectory)
+def make_target(
+    sample: ReplaySample,
+    start_idx,
+    discount,
+    num_unroll_steps,
+    num_td_steps,
+    num_stacked_frames,
+):
+    # assert not batched
+    assert len(sample.reward.shape) == 1
+
+    last_step_idx = sample.is_last.argmax()
+    collected = []
+    for curr_idx in range(start_idx, start_idx + num_unroll_steps + 1):
+        bootstrap_idx = curr_idx + num_td_steps
+        if bootstrap_idx <= last_step_idx:
+            value = sample.root_value[bootstrap_idx] * discount ** num_td_steps
+        else:
+            value = 0
+
+        for i, reward in enumerate(sample.reward[curr_idx + 1 : bootstrap_idx + 1]):
+            value += reward * discount ** i
+
+        if curr_idx <= last_step_idx:
+            last_reward = sample.reward[curr_idx]
+        else:
+            last_reward = 0
+
+        frame_idx_lower = max(curr_idx - num_stacked_frames, 0)
+        frame = sample.frame[frame_idx_lower : curr_idx + 1]
+        num_frames_to_pad = num_stacked_frames - frame.shape[0]
+        if num_frames_to_pad > 0:
+            padding_shape = (num_frames_to_pad,) + frame.shape[1:]
+            frame = np.concatenate((np.zeros(shape=padding_shape), frame))
+
+        collected.append((frame, value, last_reward))
+
+    collected_stacked = tree_utils.stack_sequence_fields(collected)
+    action = sample.action[start_idx : start_idx + num_unroll_steps]
+    child_visits = sample.child_visits[start_idx : start_idx + num_unroll_steps + 1]
+
+    return TrainTarget(
+        frame=collected_stacked[0],
+        action=action,
+        value=collected_stacked[1],
+        last_reward=collected_stacked[2],
+        child_visits=child_visits,
+    )
--- tests/test_replay.py
+++ tests/test_replay.py
@@ -1,67 +1,68 @@
-# from absl.testing import absltest
-import typing
-
-import chex
-import dm_env
-import reverb
+import tree
 import moozi as mz
 import numpy as np
-import tree
-from absl.testing import parameterized
-from acme import specs as acme_specs
-from acme.adders.reverb import test_utils as acme_test_utils
-from moozi.adder import MooZiAdder
-from moozi.types import Action
-from moozi.utils import MooZiObservation, MooZiTrainTarget
-
-TEST_CASES = [
-    dict(
-        testcase_name="BoardGameN1",
-        num_unroll_steps=0,
-        num_stacked_images=1,
-        num_td_steps=0,
-        discount=1,
-        observations=[
-            MooZiObservation(
-                dm_env.restart(0), root_value=0, child_visits=[0.1, 0.3, 0.6]
-            ),
-            MooZiObservation(
-                dm_env.transition(reward=0, observation=1, discount=0.5),
-                root_value=10,
-                child_visits=[3, 4, 5],
-            ),
-        ],
-        targets=[MooZiTrainTarget()],
-    ),
-]
+import pytest
+from numpy.testing import assert_allclose
 
 
-def _numeric_to_spec(x: typing.Union[float, int, np.ndarray]):
-    if isinstance(x, np.ndarray):
-        return acme_specs.Array(shape=x.shape, dtype=x.dtype)
-    elif isinstance(x, (float, int)):
-        return acme_specs.Array(shape=(), dtype=type(x))
-    else:
-        raise ValueError(f"Unsupported numeric: {type(x)}")
+TEST_CASES = []
+
+_sample = mz.replay.ReplaySample(
+    frame=[[11], [22], [33], [44], [55], [66]],
+    reward=[0, 200, 300, 400, 500, 600],
+    is_first=[True, False, False, False, False, False],
+    is_last=[False, False, False, False, False, True],
+    action=[101, 102, 103, 104, 105, 106],
+    root_value=[-10, -20, -30, -40, -50, -1],
+    child_visits=[np.arange(i, i + 3) / sum(list(range(i, i + 3))) for i in range(6)],
+).cast()
+
+TEST_CASES.extend(
+    [
+        dict(
+            sample=_sample,
+            start_idx=0,
+            discount=0.5,
+            num_unroll_steps=1,
+            num_td_steps=1,
+            num_stacked_frames=2,
+            expected_target=mz.replay.TrainTarget(
+                frame=[[[0], [11]], [[11], [22]]],
+                action=[101],
+                value=[200 + (-20) * 0.5, 300 + (-30) * 0.5],
+                last_reward=[0, 200],
+                child_visits=[_sample.child_visits[0], _sample.child_visits[1]],
+            ).cast(),
+        )
+    ]
+)
 
 
+@pytest.mark.kwparametrize(*TEST_CASES)
+def test_make_target(
+    sample,
+    start_idx,
+    discount,
+    num_unroll_steps,
+    num_td_steps,
+    num_stacked_frames,
+    expected_target,
+):
+    computed_target = mz.replay.make_target(
+        sample,
+        start_idx,
+        discount,
+        num_unroll_steps,
+        num_td_steps,
+        num_stacked_frames,
+    )
 
+    def _assert_shape(path, computed, expected):
+        assert computed.shape == expected.shape
 
-# class MooZiAdderTest(acme_test_utils.AdderTestMixin, parameterized.TestCase):
-#     @parameterized.named_parameters(*TEST_CASES)
-#     def test_adder(
-#         self,
-#         num_unroll_steps,
-#         num_stacked_images,
-#         num_td_steps,
-#         discount,
-#         observations,
-#         targets,
-#     ):
-#         adder = MooZiAdder(
-#             self.client,
-#             num_unroll_steps=num_unroll_steps,
-#             num_stacked_images=num_stacked_images,
-#             num_td_steps=num_td_steps,
-#             discount=discount,
-#         )
+    def _assert_content(path, computed, expected):
+        assert_allclose(computed, expected)
+
+    tree.assert_same_structure(computed_target, expected_target)
+    tree.map_structure_with_path(_assert_shape, computed_target, expected_target)
+    tree.map_structure_with_path(_assert_content, computed_target, expected_target)

f15e5ce7700c5671ae3a88b2a1ba453429d7d75c
uduse <i@uduse.com>
2021-08-30 06:48:40 +0000

    Update.

--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -1,45 +1,42 @@
 # %%
 import copy
-import chex
+import functools
 import math
+import uuid
+from dataclasses import dataclass, field
 from typing import Any, Dict, NamedTuple, Optional
 
+import anytree
+import chex
+import jax
 import jax.numpy as jnp
 import numpy as np
-from moozi.nn import NeuralNetwork, NeuralNetworkOutput
-
+import rlax
+from moozi.nn import NeuralNetwork, NeuralNetworkOutput, NeuralNetworkSpec, get_network
 from moozi.policies.policy import PolicyFeed, PolicyFn, PolicyResult
-
-# root = Node(0)
-# current_observation = game.make_image(-1)
-# expand_node(
-#     root,
-#     game.to_play(),
-#     game.legal_actions(),
-#     network.initial_inference(current_observation),
-# )
-# add_exploration_noise(config, root)
-
-# # We then run a Monte Carlo Tree Search using only action sequences and the
-# # model learned by the network.
-#     run_mcts(config, root, game.action_history(), network)
 
 
 # %%
-@chex.dataclass
-class Node:
+@dataclass
+class Node(object):
     prior: float
-    parent: Any  # Optional[Node]
-    children: dict
-    value_sum: float
-    visit_count: int
-    last_reward: float
-    hidden_state: jnp.ndarray
+    parent: Optional["Node"] = None
+    children: dict = field(default_factory=dict)
+    value_sum: float = 0.0
+    visit_count: int = 0
+    reward: float = 0.0
+    hidden_state: Optional[np.ndarray] = None
 
     @property
     def value(self) -> float:
-        assert self.visit_count > 0
-        return self.value_sum / self.visit_count
+        if self.visit_count == 0:
+            return 0
+        else:
+            return self.value_sum / self.visit_count
+
+    @property
+    def is_expanded(self) -> bool:
+        return len(self.children) > 0
 
 
 def ucb_score(parent: Node, child: Node):
@@ -53,68 +50,115 @@ def ucb_score(parent: Node, child: Node):
     prior_score = pb_c * child.prior
 
     if child.visit_count > 0:
-        value_score = child.last_reward + discount * get_value(child)
+        value_score = child.reward + discount * child.value
     else:
         value_score = 0.0
     return prior_score + value_score
 
 
 # %%
-node = Node(
-    prior=0.0,
-    parent=None,
-    children={},
-    value_sum=0.0,
-    visit_count=0,
-    last_reward=0.0,
-    hidden_state=np.array(0),
+def get_uuid():
+    return uuid.uuid4().hex[:8]
+
+
+def convert_to_anytree(node: Node, anytree_root=None, action="_"):
+    anytree_child = anytree.Node(
+        id=get_uuid(),
+        name=action,
+        parent=anytree_root,
+        prior=node.prior,
+        reward=np.round(node.reward, 3),
+        value=np.round(node.value, 3),
+        visits=np.round(node.visit_count, 3),
+    )
+    for action, child in node.children.items():
+        convert_to_anytree(child, anytree_child, action)
+    return anytree_child
+
+
+def nodeattrfunc(node):
+    return f'label="V: {node.value:.3f}\nN: {node.visits}"'
+
+
+def edgeattrfunc(parent, child):
+    return f'label="A: {child.name} \np: {child.prior:.3f}\nR: {child.reward:.3f}"'
+
+
+_partial_exporter = functools.partial(
+    anytree.exporter.UniqueDotExporter,
+    nodenamefunc=lambda node: node.id,
+    nodeattrfunc=nodeattrfunc,
+    edgeattrfunc=edgeattrfunc,
 )
+
+
+def anytree_to_png(anytree_root, file_path):
+    _partial_exporter(anytree_root).to_picture(file_path)
+
+
 # %%
-
-
-def expand_node(node: Node):
-    pass
+def expand_node(node: Node, legal_actions_mask, network_output: NeuralNetworkOutput):
+    node.hidden_state = network_output.hidden_state
+    node.reward = network_output.reward
+    action_probs = rlax.safe_epsilon_softmax(1e-7, 1).probs(
+        network_output.policy_logits
+    )
+    action_probs *= legal_actions_mask
+    action_probs /= np.sum(action_probs)
+    for action, prob in enumerate(action_probs):
+        node.children[action] = Node(
+            prior=prob,
+            parent=node,
+            children={},
+            value_sum=0.0,
+            visit_count=0,
+            reward=0.0,
+            hidden_state=None,
+        )
 
 
 def select_child(node: Node):
-    pass
+    scores = [
+        (ucb_score(node, child), action, child)
+        for action, child in node.children.items()
+    ]
+    _, action, child = max(scores)
+    return action, child
 
 
-def is_expanded(node: Node):
-    return node.visit_count > 0
+def add_exploration_noise(node: Node):
+    # TODO: adjustable later
+    dirichlet_alpha = 0.25
+    frac = 0.25
+
+    actions = list(node.children.keys())
+    noise = np.random.dirichlet([dirichlet_alpha] * len(actions))
+    for a, n in zip(actions, noise):
+        node.children[a].prior = node.children[a].prior * (1 - frac) + n * frac
 
 
-# def copy_node(node: Node) -> Node:
-#     return Node(
-#         prior=copy.deepcopy(node.prior),
-#         parent=copy.deepcopy(node.parent),
-#         children=copy.deepcopy(node.children),
-#         value_sum=copy.deepcopy(node.value_sum),
-#         visit_count=copy.deepcopy(node.visit_count),
-#         last_reward=copy.deepcopy(node.last_reward),
-#         hidden_state=copy.deepcopy(node.hidden_state),
-#     )
-
-
+# %%
 def backpropagate(leaf: Node, value: float, discount: float):
     node = leaf
-    while node.parent:
-        node = node.parent
+    while True:
         node.value_sum += value
         node.visit_count += 1
-        value = node.last_reward + value * discount
+        value = node.reward + value * discount
+        if node.parent:
+            node = node.parent
+        else:
+            break
 
 
-def make_root_node(hidden_state: jnp.ndarray) -> Node:
-    return Node(
-        prior=0.0,
-        parent=None,
-        children={},
-        value_sum=0.0,
-        last_reward=0.0,
-        visit_count=0,
-        hidden_state=hidden_state,
-    )
+# # %%
+# anytree_root = convert_to_anytree(node)
+
+# # %%
+# print(anytree.RenderTree(anytree_root))
+# anytree_to_png(anytree_root, "./policy_tree.png")
+# from IPython.display import Image
+
+# Image("./policy_tree.png")
 
 
 class MonteCarloTreeSearchResult(NamedTuple):
@@ -122,21 +166,55 @@ class MonteCarloTreeSearchResult(NamedTuple):
 
 
 class MonteCarloTreeSearch(object):
-    def __init__(self, network: NeuralNetwork, num_simulations: int) -> None:
+    def __init__(
+        self, network: NeuralNetwork, num_simulations: int, discount: float
+    ) -> None:
         super().__init__()
         self._network = network
         self._num_simulations = num_simulations
+        self._discount = discount
 
     def __call__(self, params, feed: PolicyFeed) -> PolicyResult:
         root_nn_output = self._network.initial_inference_unbatched(
             params, feed.stacked_frames
         )
-        root = make_root_node(root_nn_output.hidden_state)
+        root = Node(0)
+        expand_node(root, feed.legal_actions_mask, root_nn_output)
+        add_exploration_noise(root)
+
         for _ in range(self._num_simulations):
             node = root
-            while is_expanded(node):
-                node = select_child(node)
-            expand_node(node)
-            backpropagate()
+            while node.is_expanded:
+                action, node = select_child(node)
 
-        return PolicyResult(action=jnp.array(0), extras={})
+            assert node.parent
+            nn_output = self._network.recurrent_inference_unbatched(
+                params, node.parent.hidden_state, action
+            )
+            expand_node(node, np.ones_like(feed.legal_actions_mask), nn_output)
+            backpropagate(node, nn_output.value, self._discount)
+
+        return PolicyResult(action=jnp.array(0), extras=dict(tree=root))
+
+
+# %%
+policy_feed = PolicyFeed(
+    stacked_frames=np.zeros((10, 10)),
+    legal_actions_mask=np.ones((3,)),
+    random_key=jax.random.PRNGKey(0),
+)
+
+# %%
+nn_spec = NeuralNetworkSpec(stacked_frames_shape=(10, 10), dim_repr=2, dim_action=3)
+network = get_network(nn_spec)
+params = network.init(jax.random.PRNGKey(0))
+mcts = MonteCarloTreeSearch(network=network, num_simulations=10, discount=0.9)
+policy_result = mcts(params, policy_feed)
+tree = policy_result.extras['tree']
+
+# %%
+from IPython.display import Image
+anytree_root = convert_to_anytree(tree)
+print(anytree.RenderTree(anytree_root))
+anytree_to_png(anytree_root, "./policy_tree.png")
+Image("./policy_tree.png")

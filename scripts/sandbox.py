5c93b3598645f5cdf1e1c63aae4f870888c3321c
uduse <i@uduse.com>
2021-08-31 07:38:28 +0000

    Update.

--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -45,8 +45,8 @@ def ucb_score(parent: Node, child: Node):
     # TODO: obviously this should be a parameter
     discount = 0.99
 
-    pb_c = jnp.log((parent.visit_count + pb_c_base + 1) / pb_c_base) + pb_c_init
-    pb_c *= jnp.sqrt(parent.visit_count) / (child.visit_count + 1)
+    pb_c = np.log((parent.visit_count + pb_c_base + 1) / pb_c_base) + pb_c_init
+    pb_c *= np.sqrt(parent.visit_count) / (child.visit_count + 1)
     prior_score = pb_c * child.prior
 
     if child.visit_count > 0:
@@ -97,12 +97,13 @@ def anytree_to_png(anytree_root, file_path):
 
 
 # %%
+safe_epsilon_softmax = jax.jit(rlax.safe_epsilon_softmax(1e-7, 1).probs, backend="cpu")
+
+
 def expand_node(node: Node, legal_actions_mask, network_output: NeuralNetworkOutput):
     node.hidden_state = network_output.hidden_state
-    node.reward = network_output.reward
-    action_probs = rlax.safe_epsilon_softmax(1e-7, 1).probs(
-        network_output.policy_logits
-    )
+    node.reward = float(network_output.reward)
+    action_probs = np.array(safe_epsilon_softmax(network_output.policy_logits))
     action_probs *= legal_actions_mask
     action_probs /= np.sum(action_probs)
     for action, prob in enumerate(action_probs):
@@ -170,50 +171,70 @@ class MonteCarloTreeSearch(object):
         self, network: NeuralNetwork, num_simulations: int, discount: float
     ) -> None:
         super().__init__()
-        self._network = network
+        # self._network = network
         self._num_simulations = num_simulations
         self._discount = discount
+        self._init_inf = jax.jit(network.initial_inference_unbatched, backend="cpu")
+        self._recurr_inf = jax.jit(network.recurrent_inference_unbatched, backend="cpu")
 
     def __call__(self, params, feed: PolicyFeed) -> PolicyResult:
-        root_nn_output = self._network.initial_inference_unbatched(
-            params, feed.stacked_frames
-        )
+        root_nn_output = self._init_inf(params, feed.stacked_frames)
         root = Node(0)
         expand_node(root, feed.legal_actions_mask, root_nn_output)
         add_exploration_noise(root)
 
         for _ in range(self._num_simulations):
-            node = root
-            while node.is_expanded:
-                action, node = select_child(node)
-
-            assert node.parent
-            nn_output = self._network.recurrent_inference_unbatched(
-                params, node.parent.hidden_state, action
-            )
-            expand_node(node, np.ones_like(feed.legal_actions_mask), nn_output)
-            backpropagate(node, nn_output.value, self._discount)
+            self._simulate_once(root, params, feed)
+            # action, node = self._select_leaf(root)
+            # assert node.parent
+            # nn_output = self._recurr_inf(params, node.parent.hidden_state, action)
+            # expand_node(node, np.ones_like(feed.legal_actions_mask), nn_output)
+            # backpropagate(node, float(nn_output.value), self._discount)
 
         return PolicyResult(action=jnp.array(0), extras=dict(tree=root))
+
+    def _select_leaf(self, root: Node):
+        node = root
+        while node.is_expanded:
+            action, node = select_child(node)
+        return action, node
+
+    def _simulate_once(self, root: Node, params, feed: PolicyFeed):
+        action, node = self._select_leaf(root)
+        assert node.parent
+        nn_output = self._recurr_inf(params, node.parent.hidden_state, action)
+        expand_node(node, np.ones_like(feed.legal_actions_mask), nn_output)
+        backpropagate(node, float(nn_output.value), self._discount)
 
 
 # %%
 policy_feed = PolicyFeed(
-    stacked_frames=np.zeros((10, 10)),
+    stacked_frames=np.zeros((1, 6 * 6)),
     legal_actions_mask=np.ones((3,)),
     random_key=jax.random.PRNGKey(0),
 )
 
 # %%
-nn_spec = NeuralNetworkSpec(stacked_frames_shape=(10, 10), dim_repr=2, dim_action=3)
+num_simulations = 10000
+nn_spec = NeuralNetworkSpec(
+    stacked_frames_shape=(1, 6 * 6),
+    dim_repr=64,
+    dim_action=3,
+    repr_net_sizes=(128, 128),
+    pred_net_sizes=(128, 128),
+    dyna_net_sizes=(128, 128),
+)
 network = get_network(nn_spec)
 params = network.init(jax.random.PRNGKey(0))
-mcts = MonteCarloTreeSearch(network=network, num_simulations=10, discount=0.9)
+mcts = MonteCarloTreeSearch(
+    network=network, num_simulations=num_simulations, discount=0.9
+)
 policy_result = mcts(params, policy_feed)
-tree = policy_result.extras['tree']
+tree = policy_result.extras["tree"]
 
 # %%
 from IPython.display import Image
+
 anytree_root = convert_to_anytree(tree)
 print(anytree.RenderTree(anytree_root))
 anytree_to_png(anytree_root, "./policy_tree.png")
